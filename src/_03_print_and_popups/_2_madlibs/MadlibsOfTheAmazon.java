4eb57eb94417a0ccc9ec1be2a49097b88f2d68dd
kajifk <kajifk@icloud.com>
2025-07-30 19:49:24 +0000

    i did madlibs

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -24,16 +24,16 @@ public class MadlibsOfTheAmazon {
 		// Hint:  You will need to add more + signs to join the variables to the other parts of the story.
 		
 		String story = 
-		"Piranhas are more [**adjective**] during the day, so cross the river at\n"  +
-		"night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"  +
-		"likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"  +
+		"Piranhas are more [**atractive**] during the day, so cross the river at\n"  +
+		"night. Piranhas are attracted to fresh [**gatorade**] and will most\n"  +
+		"likely take a bite out of your [**dih**] if you [**verb**]. Whatever\n"  +
 		"you do, if you have an open wound, try to find another way to get\n"  +
 		"back to the [**place**]. Good luck!";
 		
 		// Make a pop-up that contains the final story. The \n escape characters add line breaks to the story. 
 		// If you need to, move them around to make your story look better in the pop-up
 		
-		// If you want to write your own Madlib story, just change the story variable and ask the player different questions.
+		System.out.println("i like goldfish");// If you want to write your own Madlib story, just change the story variable and ask the player different questions.
 
 	}
 }
